6f8fc3e6de00f27488e1888011ac5cdca4a32ef8
Etienne Bourganel <etienne.bourganel@gmail.com>
2021-08-30 15:28:26 +0200

    Lift states up to fix restart of the game issue

--- src/App.js
+++ src/App.js
@@ -6,20 +6,54 @@ import TilesBox from "./components/TilesBox/TilesBox"
 import generateTileArray from "./modules/generateTileArray"
 
 const App = () => {
-  const [tileArray, setTileArray] = useState([])
-
   const numberOfTiles = 16
+  const [tileArray, setTileArray] = useState([])
+  const [correctTilesNames, setCorrectTilesNames] = React.useState([])
+  const [activeTiles, setActiveTiles] = React.useState([])
+
+  const pushTile = (tile) => {
+    if (activeTiles.length < 2) {
+      if (activeTiles.length === 1 && tile.key === activeTiles[0].key) {
+        return
+      } else {
+        setActiveTiles([...activeTiles, tile])
+      }
+    } else if (activeTiles.length === 2) {
+      compareTiles()
+      setActiveTiles([tile])
+    }
+  }
+
+  const compareTiles = () => {
+    return activeTiles[0].name === activeTiles[1].name
+      ? setCorrectTilesNames([...correctTilesNames, activeTiles[1].name])
+      : false
+  }
+
+  const showImage = (tileInfoObj) => {
+    return correctTilesNames.includes(tileInfoObj.name) ||
+      (!!activeTiles[0] && tileInfoObj.key === activeTiles[0].key) ||
+      (!!activeTiles[1] && tileInfoObj.key === activeTiles[1].key)
+      ? true
+      : false
+  }
 
   const createNewTileBox = () => {
     const cardsArray = generateTileArray(numberOfTiles)
     setTileArray(cardsArray)
+    setCorrectTilesNames([])
+    setActiveTiles([])
   }
 
   return (
     <div>
       <Header />
       <StartGame createNewTileBox={createNewTileBox} />
-      <TilesBox tileArray={tileArray} />
+      <TilesBox
+        tileArray={tileArray}
+        pushTileAbove={pushTile}
+        showImage={showImage}
+      />
     </div>
   )
 }
